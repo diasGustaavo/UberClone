7a81db96922ee9f437816ed932913b4136c456b3
diasGustaavo <diasgustaavo@icloud.com>
2023-01-31 14:54:23 -0300

    add LocationSearchViewModel

--- UberClone/Core/LocationSearch/View/LocationSearchView.swift
+++ UberClone/Core/LocationSearch/View/LocationSearchView.swift
@@ -10,11 +10,11 @@ import SwiftUI
 struct LocationSearchView: View {
     @State private var startLocationText = ""
     @State private var destinationLocationText = ""
+    @StateObject var viewModel = LocationSearchViewModel()
     
     var body: some View {
         VStack {
             //  header view
-            
             HStack {
                 VStack {
                     Circle()
@@ -36,7 +36,7 @@ struct LocationSearchView: View {
                         .background(Color(.systemGroupedBackground))
                         .padding(.trailing)
                     
-                    TextField("Where to?", text: $destinationLocationText)
+                    TextField("Where to?", text: $viewModel.queryFragment)
                         .frame(height: 32)
                         .background(Color(.systemGray4))
                         .padding(.trailing)
@@ -49,11 +49,10 @@ struct LocationSearchView: View {
                 .padding(.vertical)
             
             // list view
-            
             ScrollView {
                 VStack(alignment: .leading) {
-                    ForEach(0 ..< 20, id: \.self) { _ in
-                        LocationSearchResultCell()
+                    ForEach(viewModel.results, id: \.self) { result in
+                        LocationSearchResultCell(title: result.title, subtitle: result.subtitle)
                     }
                 }
             }
---
+++ UberClone/Core/LocationSearch/ViewModel/LocationSearchViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  LocationSearchViewModel.swift
+//  UberClone
+//
+//  Created by Gustavo Dias on 31/01/23.
+//
+
+import Foundation
+import MapKit
+
+class LocationSearchViewModel: NSObject, ObservableObject {
+    //MARK: - Properties
+    
+    @Published var results = [MKLocalSearchCompletion]()
+    private let searchCompleter = MKLocalSearchCompleter()
+    
+    var queryFragment: String = "" {
+        didSet {
+            searchCompleter.queryFragment = queryFragment
+        }
+    }
+    
+    override init() {
+        super.init()
+        searchCompleter.delegate = self
+        searchCompleter.queryFragment = queryFragment
+    }
+}
+
+//MARK: - MKLocalSearchCompleterDelegate
+
+extension LocationSearchViewModel: MKLocalSearchCompleterDelegate {
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        self.results = completer.results
+    }
+}
