ddc4c99c0a404a6e21e0242e50d6d828cb83a94b
diasGustaavo <diasgustaavo@icloud.com>
2023-01-31 10:59:38 -0300

    add MapViewRepresentable & LocationManager

---
+++ UberClone/Core/Views/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//  UberClone
+//
+//  Created by Gustavo Dias on 31/01/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        UberMapViewRepresentable()
+            .ignoresSafeArea()
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
