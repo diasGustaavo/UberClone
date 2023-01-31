0aea633279afbe446bb59bba0fa725cdc3b7b684
diasGustaavo <diasgustaavo@icloud.com>
2023-01-31 10:23:15 -0300

    Initial Commit

---
+++ UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Gustavo Dias on 31/01/23.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
