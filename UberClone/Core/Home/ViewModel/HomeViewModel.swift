0f5c7a97f38b7e7a03079c402214728183a25e31
diasGustaavo <diasgustaavo@icloud.com>
2023-02-09 14:26:13 -0300

    add fetchTrips function for drivers

--- UberClone/Core/Home/ViewModel/HomeViewModel.swift
+++ UberClone/Core/Home/ViewModel/HomeViewModel.swift
@@ -74,8 +74,12 @@ class HomeViewModel: NSObject, ObservableObject {
             .sink { user in
                 self.currentUser = user
                 guard let user = user else { return }
-                guard user.accountType == .passenger else { return }
-                self.fetchDrivers()
+                
+                if user.accountType == .passenger {
+                    self.fetchDrivers()
+                } else {
+                    self.fetchTrips()
+                }
             }
             .store(in: &cancellables)
     }
@@ -120,7 +124,17 @@ extension HomeViewModel {
 //MARK: - Driver API
 
 extension HomeViewModel {
-    
+    func fetchTrips() {
+        guard let currentUser = currentUser else { return }
+        Firestore.firestore().collection("trips")
+            .whereField("driverUid", isEqualTo: currentUser.uid)
+            .getDocuments { snapshot, _ in
+                guard let documents = snapshot?.documents, let document = documents.first else { return }
+                guard let trip = try? document.data(as: Trip.self) else { return }
+                
+                print("DEBUG: Trip request for driver is \(trip)")
+            }
+    }
 }
 
 //MARK: - Location Search Helpers
